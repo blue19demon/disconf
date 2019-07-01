caddb10cee445f51f68c34262ca99c43b7defc30
zhangbo <363230482@qq.com>
2019-07-01 14:46:19 +0800

    多环境配置

--- boot-apollo/src/main/java/com/apollo/BootApolloApplication.java
+++ boot-apollo/src/main/java/com/apollo/BootApolloApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 
 @SpringBootApplication
-@EnableApolloConfig(value = {"TEST1.apollo"})
+@EnableApolloConfig /* (value = {"TEST1.apollo"}) */
 public class BootApolloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootApolloApplication.class, args);
